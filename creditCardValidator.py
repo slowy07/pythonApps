3475d4bd3066a317226a1920953beed002974f69
slowy07 <slowy.arfy@gmail.com>
2020-10-06 21:11:52 +0700

    finishing

--- creditCardValidator.py
+++ creditCardValidator.py
@@ -25,3 +25,51 @@ class creditCard:
             comp = 'Gasoline Card'
         
         return 'Company : ' +comp
+
+#create first Check
+def firstCheck(self):
+    if 13 <= len(self.cardNo) <= 19:
+        message = "First check : valid items in length"
+
+    else:
+        message = "First check : check card number be of 13 or 16 digits long"
+    
+    return message
+
+def validate(self):
+    sum_ = 0
+    cardNo_ = self.cardNo[::-1]
+    for i in range(len(cardNo_)):
+        if i % 2 == 1:
+            doubleIt = int(cardNo_[i]) * 2
+            if len(str(doubleIt)) == 2:
+                sum_ += sum([eval(i) for i in str(doubleIt)])
+            else:
+                sum_ += doubleIt
+        else:
+            sum_ += int(cardNo_[i])
+        
+    if sum_ % 10 == 0:
+        response = "Valid Card"
+    else:
+        response = "Invalid Card"
+    
+    return response
+
+
+@property
+def checksum(self):
+    return '#CHECKSUM : '+self.cardNo_[-1]
+    
+@classmethod
+def setCard(cls, cardToCheck):
+    return cls(cardToCheck)
+
+
+card_number = input()
+card = creditCard.setCard(card_number)
+print(card.company)
+print('Card :', card.cardNo_)
+print(card.firstCheck())
+print(card.checksum)
+print(card.validate())
