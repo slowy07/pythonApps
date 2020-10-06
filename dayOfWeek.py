3df0b705d030de6bd9be24a3b49f41f693b4030e
slowy07 <slowy.arfy@gmail.com>
2020-10-06 21:28:13 +0700

    add find day of date

--- dayOfWeek.py
+++ dayOfWeek.py
@@ -9,7 +9,10 @@ def processDate(userInput):
     
     return userInput
 
+def findDay(date):
+    born = datetime.datetime.strptime(date, '%d %m %Y').weekday()
+    return (calendar.day_name[born])
 
 userInput = str(input("enter date : "))
 date = processDate(userInput)
-print("Day On : "+userInput)
+print("Day On : "+userInput +" is "+findDay(date))
