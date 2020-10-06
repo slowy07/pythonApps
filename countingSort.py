f6756ab177cc83551470abb4b8d37fa5515f8450
slowy07 <slowy.arfy@gmail.com>
2020-10-06 14:59:55 +0700

    fixing ouput print(flist)

--- countingSort.py
+++ countingSort.py
@@ -4,8 +4,24 @@ tlist = list(map(int, input().split()))
 k = max(tlist)
 number = len(tlist)
 
-def countingSort():
+def countingSort(tlist, k, number):
     """
     create counting sort
     """
+    countList = [0] * (k+1)
     
+    for i in range(0, number):
+        countList[tlist[i]] += 1
+
+    for i in range(1, k+1):
+        countList[i] = countList[i] + countList[i - 1]
+
+    flist = [0] * (number)
+    for i in range(number - 1, -1, -1):
+        countList[tlist[i]] = countList[tlist[i]] - 1
+        flist[countList[tlist[1]]] = (tlist[i])
+
+        return True
+    
+    flist = countingSort(tlist, k, number)
+    print(flist)
