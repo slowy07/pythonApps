b81a1298bc8e9b61c0742e4a4fbd3072056a461f
slowy07 <slowy.arfy@gmail.com>
2020-10-09 23:59:31 +0700

    create linked list class

--- mergeLinkedList.py
+++ mergeLinkedList.py
@@ -3,3 +3,20 @@ class node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class linkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data, end="->")
+            temp = temp.next
+
+    def append(self, newData):
+        newNode = node(newData)
+        if self.head is True:
+            self.head = newNode
+            return
