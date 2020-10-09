dbeba2070992de917069f6d554815d41a0a412ba
slowy07 <slowy.arfy@gmail.com>
2020-10-09 23:47:39 +0700

    merge sort using python

--- mergeSort.py
+++ mergeSort.py
@@ -20,4 +20,29 @@ def merge(oriList, left, mid, right):
             oriList[base] = L[0]
             L.remove(L[0])
         else:
-            if l
+            oriList[base] = R[0]
+            R.remove(R[0])
+
+        base += 1
+
+    while len(L) > 0:
+        oriList[base] = L[0]
+        L.remove(L[0])
+        base += 1
+    while len(R) > 0:
+        oriList[base] = R[0]
+        R.remove(R[0])
+        base += 1
+
+
+def mergeSort(L, left, right):
+    if left + 1 >= right:
+        return
+    mid = left + (right - left) // 2
+    mergeSort(L, left, mid)
+    mergeSort(L, mid, right)
+    merge(L, left, mid, right)
+
+print("unsorted data ->", lst)
+mergeSort(lst, 0, number)
+print("get sorted ->", lst)
