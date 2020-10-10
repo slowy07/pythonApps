efd7fa743fb055fc5b2b8f645fcea9b3ac433828
slowy07 <slowy.arfy@gmail.com>
2020-10-10 15:50:22 +0700

    implementation mysql database  on python

---
+++ mysqlDatabase.py
@@ -0,0 +1,16 @@
+#simple database mysql using python
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = "0.0.0.0",
+    user = "root",
+    password = "",
+    databaseName = "myDatabase"
+)
+setCursor = database.cursor()
+setCursor.execute("SELECT column FROM table")
+
+getResult = setCursor.fetchall()
+
+for x in getResult:
+    print(x)
