4dc9ad015818402858ae0a9828f841cc2894a790
slowy07 <slowy.arfy@gmail.com>
2020-10-10 15:15:14 +0700

    mp3 player

--- mp3Player.py
+++ mp3Player.py
@@ -13,11 +13,76 @@ listOfSong = []
 realNames = []
 
 v = StringVar()
-songLabel = Label(frameWindow, textVariable=v, width=35)
+songLabel = Label(frameWindow, textvariable=v, width=35)
 index = 0
 
 def directoryChooser():
     directory = askdirectory()
     os.chdir(directory)
 
+    for files in os.listdir(directory):
+        if files.endswith(".mp3"):
+            realdir = os.path.realpath(files)
+            audio = ID3(realdir)
+            realNames.append(audio['TIT2'].text[0])
+            
+            
+            listOfSong.append(files)
     
+    pygame.mixer.init()
+    pygame.mixer.music.load(listOfSong[0])
+    
+directoryChooser()
+def updateLabel():
+    global index
+    v.set(realNames[index])
+    #return songName
+
+def nextSong(event):
+    global index
+    index += 1
+    pygame.mixer.music.load(listOfSong[index])
+    pygame.mixer.music.play()
+    updateLabel()
+
+def prevSong(event):
+    global index
+    index -= 1
+    pygame.mixer.music.load(listOfSong[index])
+    pygame.mixer.music.play()
+    updateLabel()
+
+def stopSong(event):
+    pygame.mixer.music.stop()
+    v.set("")
+    #return songName
+
+label = Label(frameWindow, text="music player")
+label.pack()
+
+
+listBox = Listbox(frameWindow)
+listBox.pack()
+
+realNames.reverse()
+
+for items in realNames:
+    listBox.insert(0, items)
+
+realNames.reverse()
+nextButton = Button(frameWindow, text="next Song")
+nextButton.pack()
+
+previousButton = Button(frameWindow, text="previous song")
+previousButton.pack()
+
+stopButton = Button(frameWindow, text="stop")
+stopButton.pack()
+
+nextButton.bind("<Button-1>", nextSong)
+previousButton.bind("<Button-1>", prevSong)
+stopButton.bind("<Button-1>", stopSong)
+
+songLabel.pack()
+
+frameWindow.mainloop()
