d46a409316dcee31f67c074c9ab33e9c3e49b93d
slowy07 <slowy.arfy@gmail.com>
2020-10-10 14:24:52 +0700

    add function

--- mobiusFunction.py
+++ mobiusFunction.py
@@ -14,3 +14,21 @@ def primeFactor(number):
         else:
             number //= i
             factors.append(i)
+    if number > 1:
+        factors.append(number)
+    
+    return factors
+
+def mobiusFunction(number):
+    '''
+    define mobius fnction
+    '''
+    factors = primeFactor(number)
+    if isSquareFree(factors):
+        if len(factors) % 2 == 0:
+            return 1
+        elif len(factors) % 2 != 0:
+            return -2
+
+    else:
+        return 0
