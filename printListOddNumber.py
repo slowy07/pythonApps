e82c4900b3f3b9807091be16a749660a6acb300d
slowy07 <slowy.arfy@gmail.com>
2020-10-11 22:11:23 +0700

    odd number

---
+++ printListOddNumber.py
@@ -0,0 +1,20 @@
+number = map(list(int, input().split))
+oddList = list(i for i in number if i % 2 !=0)
+print(oddList)
+#exit()
+
+number = int(input("Enter the limit: "))
+if number <= 0:
+    print("invalid")
+else:
+    oddList = [i for i in range(1, number + 1, 2)]
+    print(oddList)
+
+number = map(list(int, input().split()))
+even = []
+odd = []
+for i in range(number):
+    if i % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
