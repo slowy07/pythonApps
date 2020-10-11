e0deab292898f404ccdaa6202514047859dafdd8
slowy07 <slowy.arfy@gmail.com>
2020-10-11 18:13:41 +0700

    json parser python

---
+++ jsonParser.py
@@ -0,0 +1,22 @@
+import json
+
+class jsonParser:
+    
+    def convertJsonToPython(self, parJsonFile):
+        with open(parJsonFile) as jsonFile:
+            dataDic = json.load(jsonFile)
+            
+        return dataDic
+
+    def convertPythonToJson(self, parDataDic, parJsonFile=""):
+
+        if parJsonFile:
+            with open(parJsonFile, "w") as outFile:
+                return json.dump(parDataDic, outFile)
+        else:
+            return json.dump(parDataDic)
+
+    def getJsonValue(self, parValue, parJsonFile):
+        dataDic = self.convertJsonToPython(parJsonFile)
+        
+        return dataDic[parValue]
