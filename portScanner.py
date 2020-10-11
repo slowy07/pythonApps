7ddfcdc7d02ca0ff5dde10234acd388f198121f6
slowy07 <slowy.arfy@gmail.com>
2020-10-11 21:44:27 +0700

    update port scanner

--- portScanner.py
+++ portScanner.py
@@ -12,3 +12,24 @@ print("-" * 60)
 print("please wait, scanning remote host", remoteServerIp)
 print("-" * 60)
 
+t1 = datetime.now()
+
+try:
+    for port in range(1, 1025):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIp, port))
+        if result == 0:
+            print("port {} : Open".format(port))
+        sock.close()
+except KeyboardInterrupt:
+    print("exiting program")
+    sys.exit()
+except socket.gaierror:
+    print("hostname could not be resolved, exiting")
+    sys.exit()
+except socket.error:
+    print("couldn't connect to server ")
+    sys.exit()
+t2 = datetime.now()
+total = t2 - t1
+print('Scanning complete:', total)
