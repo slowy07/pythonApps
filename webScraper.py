270cb80133d5f5fb69d64d2ecd622091878b1e08
slowy07 <slowy.arfy@gmail.com>
2020-10-13 18:58:22 +0700

    python web scrapping

---
+++ webScraper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+url = '''url to scrapping'''
+driver = webdriver.Chrome('./chromedriver')
+driver.get(url)
+time.sleep(5)
+
+
+htmlWeb = driver.page_source
+soup = BeautifulSoup(htmlWeb, "html.parser")
+allInformation = soup.find('div',{'id':'namesearch'})
+result = allInformation.find_all('a')
+
+count = 0
+for results in result:
+    print(results.text)
+    count = count + 1
+    if count == 10:
+        break 
+
+driver.close()
