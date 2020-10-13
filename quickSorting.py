3730230259fb66cf350f96e084eb42b0e13137ad
slowy07 <slowy.arfy@gmail.com>
2020-10-13 08:31:03 +0700

    python quick sorting

---
+++ quickSorting.py
@@ -0,0 +1,23 @@
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quickSorting(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSorting(arr, low, pi - 1)
+        quickSorting(arr, pi + 1, high)
+
+arr = [10,9,8,7,2,3,5,2]
+print("array is ",arr)
+numberLength = len(arr)
+quickSorting(arr, 0, numberLength - 1)
+print("sorted array is ",arr)
