5e9829e906930284b8d81ce42c19026689e69f91
slowy07 <slowy.arfy@gmail.com>
2020-10-13 19:06:56 +0700

    fixing filename error

---
+++ backupAutomater.py
@@ -0,0 +1,21 @@
+import datetime
+import os
+import shutil
+
+timeToday = datetime.time.today()
+setStringDate = timeToday.isoformat()
+
+
+configDir = os.getenv("my_config")
+dropbox = os.getenv("dropbox")
+configFile = 'services.conf'
+cofigFilename = os.path.join(configDir, configFile)
+sourceDir = os.path.expanduser('~/Library/Services/')
+destinationDir = os.path.join(dropbox, "myBackups"+"/"+"AutomaterServices"+setStringDate+"/")
+
+for detFilename in open(cofigFilename):
+    fileName = detFilename.strip()
+    if fileName:
+        sourceFile = os.path.join(sourceDir, fileName)
+        destFilename = os.path.join(destinationDir, fileName)
+        shutil.copytree(sourceFile, destFilename)
