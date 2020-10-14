7675acc7542929c262e8e5e32ad9dbd3ebc80862
slowy07 <slowy.arfy@gmail.com>
2020-10-14 11:44:53 +0700

    fixing request data

---
+++ googleMaps.py
@@ -0,0 +1,19 @@
+import geocoder
+import json
+import requests
+
+
+geoLocation = geocoder.ip('me')
+
+latitude = geoLocation.latlng[0]
+longitude = geoLocation.latlng[1]
+
+query = input("enter query")
+key = '''api key'''
+url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + \
+    str(latitude) + "," + str(longitude) + "radius=1000"
+
+requestData = requests.get(url + 'query=' + query + '&key=' + key)
+x = requestData.json()
+y = x['results']
+print(y)
