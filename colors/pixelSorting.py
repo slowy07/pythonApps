dc42ece7ea0f57d30a08719af7994f89533f7e24
slowy07 <slowy.arfy@gmail.com>
2020-10-15 20:35:28 +0700

    export to excel

--- colors/pixelSorting.py
+++ colors/pixelSorting.py
@@ -9,3 +9,26 @@ from tqdm import tqdm
 import sound
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", required=True, help="Enter filename of your picture : ")
+#parser.add_argument("-s", required=True, help="Speed factor of the audio to be increased or decreased")
+#parser.add_argument("-av", required=True, help="Speed factor of the audio visualizer to be increased or decreased")
+
+args = parser.parse_args()
+os.makedirs("Image_sort/"+str(args.f))
+print(str(args.f).capitalize()+" directory is created!")
+
+df =[]
+total = 0
+dict, final, imgList = {}, [], []
+
+def createDataSet(val = 0 , data =[]):
+    global dict
+    dict[len(data)] = data
+    if val != 0:
+        if val == max(dict.keys()):
+            finalDf = pd.DataFrame(dict[val], columns=["Blue","Green","Red"])
+            finalDf.to_excel("Image_sort/"+str(args.f)+"/"+"ouput.xls")
+
+
+
