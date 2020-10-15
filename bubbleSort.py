7b634cf72b234af23415a2099fee5219e064f33b
slowy07 <slowy.arfy@gmail.com>
2020-10-16 01:12:02 +0700

    bubble sort (finishing)

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,6 +3,19 @@ def bubbleSort(arr):
     
     for i in range(number):
         notSwap = True
-        for j in range(0, n - i - 1):
+        for j in range(0, number - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                if arr[j] > arr[j +1]:
+                    arr[j], arr[j+1] = arr[j + 1], arr[j]
+                    notSwap = False
+                    
+        if notSwap:
+            break
+        
+arr = [64, 22, 32, 21, 82, 11]
+
+bubbleSort(arr)
+print("sorted array ")
+for i in range(len(arr)):
+    print("%d" %arr[i])
