e3e097c061ac4f9a0ffc4ca203ae8e72492b508f
slowy07 <slowy.arfy@gmail.com>
2020-10-16 23:36:26 +0700

    webcam

---
+++ webcamPython.py
@@ -0,0 +1,23 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+framesWidth = int(capture.get(3))
+framesHeight = int(capture.get(4))
+
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+output = cv2.VideoWriter('recording.avi', fourcc, 20.0, (framesWidth, framesHeight))
+
+while True:
+    ret, frame = capture.read()
+    if ret == True:
+        out.write(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+
+#CREATE 
+capture.release()
+out.release()
+cv2.destroyAllWindows()
