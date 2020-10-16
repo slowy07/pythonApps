eb5c48921c5050be87b9dbef4796f89af3b944be
slowy07 <slowy.arfy@gmail.com>
2020-10-17 00:12:57 +0700

    schedullar

---
+++ callScheduller/scheduller.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from caller import search
+
+sched = BlockingScheduler()
+
+
+#set 2 hours
+sched.add_job(search, 'interval',hours = 1)
+sched.start()
