eebcc7ebc307a04aa685d2574547de068a8d1f63
slowy07 <slowy.arfy@gmail.com>
2020-10-18 07:43:39 +0700

    pigeon hole sorting python

---
+++ sortingAlgorithm/pigeonHoleSort.py
@@ -0,0 +1,20 @@
+def pigeonHoleSort(nums):
+    minNumbers = min(nums)
+    maxNumbers = max(nums)
+
+    size = maxNumbers - minNumbers + 1
+    holes = [0] * size
+
+    for x in nums:
+        holes[x - minNumbers] += 1
+
+    i = 0
+    for count in range(size):
+        while holes[count] > 0:
+            holes[count] -= 1
+            nums[i] = count + minNumbers
+            i += 1
+
+nums = [12,26,77,22,88,1]
+print(pigeonHoleSort(nums))
+print(nums)
