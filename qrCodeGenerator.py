94b63f866495a1b7c222855822de8cafbce78135
slowy07 <slowy.arfy@gmail.com>
2020-10-20 07:45:20 +0700

    qr code generator
    Co-Authored-By: Zulfikri Ramadhan Yoga <ctrlbzul5@users.noreply.github.com>

---
+++ qrCodeGenerator.py
@@ -0,0 +1,10 @@
+import pyqrcode
+import PIL
+from pyqrcode import QRCode
+
+generateData = input("enter text to convert :")
+imageName = input("enter image name to save :")
+imageNameResult = imageName + ".png"
+url = pyqrcode.create(generateData)
+
+url.png(imageNameResult, scale = 6)
