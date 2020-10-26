eabe72c948183c994d10399681436b5ec8666a06
slowy07 <slowy.arfy@gmail.com>
2020-10-26 21:05:40 +0700

    binnary and operation

---
+++ bitManipulation/binerAndOperation.py
@@ -0,0 +1,16 @@
+def binaryAnd(a: int, b: int):
+    if a < 0 and b < 0:
+        raise ValueError("the value integer mus be positive")
+
+    aBinary = str(bin(a))[2:]
+    bBinary = str(bin(b))[2:]
+    maxLength = max(len(aBinary), len(bBinary))
+
+    return "0b"+"".join(
+        str(int(char_a == "1" and char_b == "1"))
+        for char_a, char_b in zip(aBinary.zfill(maxLength), bBinary.zfill(maxLength))
+    )
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
