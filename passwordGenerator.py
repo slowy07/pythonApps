3acedb3c8ce5824787fb776fb02327218cac22d7
slowy07 <slowy.arfy@gmail.com>
2020-10-27 00:44:52 +0700

    password generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,5 @@
 import string as str
-import secret
+import secrets
 import random
 
 class passwordGenerator():
@@ -16,3 +16,69 @@ class passwordGenerator():
                 pass
 
         return sequence
+
+    @staticmethod
+    def generatePassword(sequence, passlength= 8):
+        password = ''.join((secrets.choice(sequence) for i in range(passlength)))
+        return password
+
+class interface():
+    hasCharacters = {
+        "lowercase": True,
+        "uppercase": True,
+        "digits": True,
+        "punctuation": True
+    }
+    @classmethod
+    def changeHasCharacter(cls, change):
+        try:
+            cls.hasCharacters[change]
+        except:
+            print("invalid")
+        else:
+            cls.hasCharacters[change] = not cls.hasCharacters[change]
+            print(f"{change} is now set to {cls.hasCharacters[change]}")
+    @classmethod
+    def showCharacters(cls):
+        print(cls.hasCharacters)
+
+
+    def generatingPassword(self, lenght):
+        sequence = passwordGenerator.genSequence(list(self.hasCharacters.values()))
+        print(passwordGenerator.generatePassword(sequence, lenght))
+
+def listToVerticalString(list):
+    toReturn = ""
+    for member in list:
+        toReturn += f"{member} \n"
+
+    return toReturn
+
+class running():
+
+    def decideOperation():
+        userInput = input("pass: ")
+        try:
+            int(userInput)
+        except:
+            interface.changeHasCharacter(userInput)
+        else:
+            interface().generatePassword(int(userInput))
+        finally:
+            print("\n\n")
+
+    def run(self):
+        menu = \
+f"""Welcome to the PassGen App!
+Commands:
+    generate password ->
+    <lenght of the password>
+commands to change the characters to be used to generate passwords:
+{list_to_vertical_string(Interface.has_characters.keys())}
+            """
+            print(menu)
+            while True:
+                self.decideOperation()
+
+
+running().run()
