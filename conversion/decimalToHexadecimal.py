694e2345bc0f167753c27f68e3afbbcc52ab576b
slowy07 <slowy.arfy@gmail.com>
2020-10-31 18:01:04 +0700

    decimal to hexa

---
+++ conversion/decimalToHexadecimal.py
@@ -0,0 +1,37 @@
+values = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "a",
+    11: "b",
+    12: "c",
+    13: "d",
+    14: "e",
+    15: "f",
+}
+
+def decimalToHexadecimal(decimal):
+    assert type(decimal) in (int, float) and decimal == int(decimal)
+    hexadecimal = ""
+    negative = False
+    if decimal < 0:
+        negative = True
+        decimal *= -1
+    while decimal > 0:
+        decimal, remainder = divmod(decimal, 16)
+        hexadecimal = values[remainder] + hexadecimal
+    hexadecimal = "0x" + hexadecimal
+    if negative:
+        hexadecimal = "-" + hexadecimal
+    return hexadecimal
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
---
+++ conversion/hexadecimalToDecimal.py
@@ -0,0 +1,19 @@
+hexTable = {hex(i)[2:]: i for i in range(16)}
+
+def hexToDecimal(hexString: str)->str:
+    hexString = hexString.strip().lower()
+    if not hexString:
+        raise ValueError("empty string was passed to the function")
+    isNegative = hexString[0] == "-"
+    if isNegative:
+        hexString = hexString[1:]
+    if not all(char in hexTable for char in hexString):
+        raise ValueError("non hexadecimal value was passed in function")
+    decimalNumber = 0
+    for char in hexString:
+        decimalNumber = 16 * decimalNumber + hexTable[char]
+    return -decimalNumber if isNegative else decimalNumber
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
