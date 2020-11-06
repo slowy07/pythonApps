5491e5bc94b9f25da813337a0e51e482c8c9209b
slowy07 <slowy.arfy@gmail.com>
2020-11-06 08:39:05 +0700

    caesar decrypt bruteforce mode

---
+++ caesarDecrypt.py
@@ -0,0 +1,19 @@
+"""
+for case if find caesar message bruteforce
+"""
+
+message = "GIEWIVrGMTLIVrHIQS" 
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(letters)):
+    translated = ''
+    for symbol in message:
+        if symbol in letters:
+            num = letters.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(letters)
+            translated = translated + letters[num]
+        else:
+            translated = translated + symbol
+print("key #%s : %s"%(key, translated))
