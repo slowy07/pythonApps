29ac46466149ae66ecbf7319ba0cf8b695502673
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-11-06 19:21:39 +0700

    check internet speed using python

---
+++ speedTest.py
@@ -0,0 +1,8 @@
+from speedtest import Speedtest #pip install speedtest-cli
+
+
+speedTest = Speedtest()
+print("donwload : ",speedTest.download())
+print("upload :",speedTest.upload())
+speedTest.get_servers([])
+print("ping : ",speedTest.results.ping + " ms")
