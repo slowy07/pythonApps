3789ec776727c5fb68bb59534e8d0a3ef9708bac
slowy07 <slowy.arfy@gmail.com>
2020-11-07 08:06:03 +0700

    reverse cryptograph

---
+++ reverseEncrypt.py
@@ -0,0 +1,10 @@
+message = "zulkepretest make simpletes cpplest"
+translate = ''
+i = len(message) - 1
+
+while i >= 0:
+    translate = translate + message[i]
+    i = i - 1
+
+print("enc message is :"+ translate)
+print("message :"+ message)
