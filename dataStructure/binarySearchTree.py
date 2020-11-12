f3a8ede91c02083972972172d4ff5c1c0e1abbd3
slowy07 <slowy.arfy@gmail.com>
2020-11-12 16:31:12 +0700

    binary search tree implementation

---
+++ dataStructure/binarySearchTree.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+class Node:
+    def __init__(self, label, parent):
+        self.label = label
+        self.left = None
+        self.right = None
+        self.parent = parent
+
+    def getLabel(self):
+        return self.label
+
+    def setLabel(self):
+        self.label = label
+    
+     def getLeft(self):
+        return self.left
+
+    def setLeft(self, left):
+        self.left = left
+
+    def getRight(self):
+        return self.right
+
+    def setRight(self, right):
+        self.right = right
+
+    def getParent(self):
+        return self.parent
+
+    def setParent(self, parent):
+        self.parent = parent
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, label):
+        new_node = Node(label, None)
