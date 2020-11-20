7c3e8df8b1cec62bbd2db7535621aa58b23c7d64
slowy07 <slowy.arfy@gmail.com>
2020-11-21 05:48:59 +0700

    array in numpy

---
+++ numpy/numpyArrayCreation.py
@@ -0,0 +1,10 @@
+import numpy as np
+ 
+matrixB = np.empty(2, dtype = int)
+print("Matrix b : \n", matrixB)
+ 
+matrixA = np.empty([2, 2], dtype = int)
+print("\nMatrix a : \n", matrixA)
+ 
+matrixC = np.empty([3, 3])
+print("\nMatrix c : \n", matrixC)
