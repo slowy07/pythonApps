fefb8bbed7c87022b9d0cce070945c576cc3e459
slowy07 <slowy.arfy@gmail.com>
2020-11-21 05:57:36 +0700

    sending html email

---
+++ sendingEmailHtml.py
@@ -0,0 +1,20 @@
+import smtplib
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: SMTP HTML e-mail test
+
+This is an e-mail message to be sent in HTML format
+
+<b>zulkepretes make simplest ostest.</b>
+<h1>zulkepretes still making simplest ostest.</h1>
+"""
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
