3ab87897b7356e94de4556e5882d931a87c845f8
slowy07 <slowy.arfy@gmail.com>
2020-11-21 09:59:11 +0700

    sha 3 256

---
+++ cryptograph/sha3_256.py
@@ -0,0 +1,14 @@
+import sys 
+import hashlib 
+
+if sys.version_info < (3, 6): 
+	import sha3 
+
+# initialize a string 
+str = "zulkepretes"
+# encode the string 
+encoded_str = str.encode() 
+# create sha3-256 hash objects 
+obj_sha3_256 = hashlib.sha3_256(encoded_str) 
+# print in hexadecimal 
+print("\nSHA3-256 Hash: ", obj_sha3_256.hexdigest()) 
