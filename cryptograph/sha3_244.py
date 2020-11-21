4a16b07ca39467e96853c024fabf511cfef034b6
slowy07 <slowy.arfy@gmail.com>
2020-11-21 09:53:32 +0700

    sha 3 244

---
+++ cryptograph/sha3_244.py
@@ -0,0 +1,16 @@
+import sys 
+import hashlib 
+  
+if sys.version_info < (3, 6): 
+    import sha3 
+  
+# initiating the "s" object to use the 
+# sha3_224 algorithm from the hashlib module. 
+s = hashlib.sha3_224() 
+# will output the name of the hashing algorithm currently in use. 
+print(s.name) 
+# will output the Digest-Size of the hashing algorithm being used. 
+print(s.digest_size) 
+# providing the input to the hashing algorithm. 
+s.update(b"zulkepretes")
+print(s.hexdigest())
