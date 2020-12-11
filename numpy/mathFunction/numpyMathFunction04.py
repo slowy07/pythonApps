26eb3c8616dcde416306c3a41b0be3db2268e773
slowy07 <slowy.arfy@gmail.com>
2020-12-11 22:01:56 +0700

    cosh numpy function

---
+++ numpy/mathFunction/numpyMathFunction04.py
@@ -0,0 +1,10 @@
+# cosh() function
+ 
+import numpy as np
+import math
+ 
+in_array = [0, math.pi / 2, np.pi / 3, np.pi]
+print ("Input array : \n", in_array)
+ 
+cosh_Values = np.cosh(in_array)
+print ("\ncosine Hyperbolic values : \n", cosh_Values)
