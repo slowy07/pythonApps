998888e5ef55dd37f4962049da0c3b5e63924fb2
slowy07 <slowy.arfy@gmail.com>
2020-12-11 22:06:11 +0700

    conj function

---
+++ numpy/mathFunction/numpyMathFunctionConj.py
@@ -0,0 +1,9 @@
+import numpy as np
+ 
+in_complx1 = 2+4j
+out_complx1 = np.conj(in_complx1)
+print ("Output conjugated complex number of  2+4j : ", out_complx1)
+ 
+in_complx2 =5-8j
+out_complx2 = np.conj(in_complx2)
+print ("Output conjugated complex number of 5-8j: ", out_complx2)
