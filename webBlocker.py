8bea737abb9562c054c30b9d21011efc03addc42
slowy07 <slowy.arfy@gmail.com>
2020-12-11 18:46:00 +0700

    implementation web block

---
+++ webBlocker.py
@@ -0,0 +1,24 @@
+from time import *  
+from datetime import *  
+  
+host_path = r"/etc/hosts"  
+redirect = "127.0.0.1"  
+websites = ["www.facebook.com", "https://www.facebook.com"]  
+  
+while True:  
+    if datetime(datetime.now().year,datetime.now().month,datetime.now().day,9)<datetime.now()<datetime(datetime.now().year,datetime.now().month,datetime.now().day,17):  
+        with open(host_path,"r+") as fileptr:  
+            content = fileptr.read()  
+            for website in websites:  
+                if website in content:  
+                    pass  
+                else:  
+                    fileptr.write(redirect+"        "+website+"\n")  
+    else:  
+        with open(host_path,'r+') as file:  
+            content = file.readlines();  
+            file.seek(0)  
+            for line in content:  
+                if not any(website in line for website in websites):  
+                    file.write(line)  
+            file.truncate()  
