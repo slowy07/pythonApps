7197e7f753642ada1d003f577c66205ab06a6ef4
slowy07 <slowy.arfy@gmail.com>
2020-12-11 22:28:30 +0700

    log() function

---
+++ numpy/mathFunction/numpyMathFunctionLog.py
@@ -0,0 +1,12 @@
+# log() function
+import numpy as np
+ 
+in_array = [1, 3, 5, 2**8]
+print ("Input array : ", in_array)
+ 
+out_array = np.log(in_array)
+print ("Output array : ", out_array)
+ 
+ 
+print("\nnp.log(4**4) : ", np.log(4**4))
+print("np.log(2**8) : ", np.log(2**8))
