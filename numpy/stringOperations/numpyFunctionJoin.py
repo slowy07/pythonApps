f1735e8d3b03db588dd49776ffcd25c99df464ad
slowy07 <slowy.arfy@gmail.com>
2020-12-21 06:38:49 +0700

    isNumeric function

---
+++ numpy/stringOperations/numpyFunctionJoin.py
@@ -0,0 +1,9 @@
+# numpy.join() function
+ 
+import numpy as np
+ 
+# splitting a string
+print(np.char.join('-', 'adit'))
+ 
+# splitting a string
+print(np.char.join(['-', ':'], ['adit', 'zulkepretes']))
