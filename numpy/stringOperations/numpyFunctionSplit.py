b606c7ee82f1096fa99f3b9876efbeeea3caf4cd
slowy07 <slowy.arfy@gmail.com>
2020-12-21 06:44:14 +0700

    split function

---
+++ numpy/stringOperations/numpyFunctionSplit.py
@@ -0,0 +1,9 @@
+# numpy.split() function
+ 
+import numpy as np
+ 
+# splitting a string
+print(np.char.split('jole arfy catur'))
+ 
+# splitting a string
+print(np.char.split('jole, arfy, catur', sep = ','))
