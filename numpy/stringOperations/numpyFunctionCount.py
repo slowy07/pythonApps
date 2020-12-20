f3770547f87f491074444d9aac512f1fc1454bc2
slowy07 <slowy.arfy@gmail.com>
2020-12-21 06:37:33 +0700

    count function

---
+++ numpy/stringOperations/numpyFunctionCount.py
@@ -0,0 +1,11 @@
+# numpy.count() function
+ 
+import numpy as np
+ 
+a=np.array(['arfy', 'slowy', 'sayutod'])
+ 
+# counting a substring
+print(np.char.count(a,'sayutod'))
+ 
+# counting a substring
+print(np.char.count(a, 'fo'))
