a79abca77c52a8817ac0432c6cb7dcfe9c198fb0
slowy07 <slowy.arfy@gmail.com>
2020-12-21 06:38:24 +0700

    function is numeric

---
+++ numpy/stringOperations/numpyFunctionIsNumeric.py
@@ -0,0 +1,10 @@
+# numpy.isnumeric() function
+ 
+import numpy as np
+ 
+ 
+# counting a substring
+print(np.char.isnumeric('arfyslowy'))
+ 
+# counting a substring
+print(np.char.isnumeric('kloter2surga'))
