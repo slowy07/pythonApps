e82f14a6d19803a6bfcd2b999217c54273515270
slowy07 <slowy.arfy@gmail.com>
2020-12-21 06:41:58 +0700

    numpy Rfind function

---
+++ numpy/stringOperations/numpyFunctionRfind.py
@@ -0,0 +1,11 @@
+# numpy.rfind() function
+ 
+import numpy as np
+ 
+a=np.array(['arfy', 'slowy', 'afdun'])
+ 
+# counting a substring
+print(np.char.rfind(a,'afdun'))
+ 
+# counting a substring
+print(np.char.rfind(a, 'fo'))
