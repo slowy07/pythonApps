7b13188cb3dcac11ec719625f340f75e97505a1f
slowy07 <slowy.arfy@gmail.com>
2020-12-23 06:53:20 +0700

    function

---
+++ numpy/linearAlgebra/numpyDotFunction.py
@@ -0,0 +1,12 @@
+import numpy as np
+ 
+# Scalars
+product = np.dot(5, 4)
+print("Dot Product of scalar values  : ", product)
+ 
+# 1D array
+vector_a = 2 + 3j
+vector_b = 4 + 5j
+ 
+product = np.dot(vector_a, vector_b)
+print("Dot Product  : ", product)
