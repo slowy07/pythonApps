69be9c0cb2fcb37a69b9a14c1981e0cb1e1d4332
slowy07 <slowy.arfy@gmail.com>
2020-12-23 06:53:54 +0700

    trace function

---
+++ numpy/linearAlgebra/numpyTraceFunction.py
@@ -0,0 +1,10 @@
+import numpy as np
+ 
+# creating an array using 
+# array method
+A = np.array([[6, 1, 1],
+              [4, -2, 5],
+              [2, 8, 7]])
+ 
+ 
+print("\nTrace of A:", np.trace(A))
