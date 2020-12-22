df8ed65e25479b98c4cd29ba26440af351773139
slowy07 <slowy.arfy@gmail.com>
2020-12-23 06:57:35 +0700

    numpy binary operation

---
+++ numpy/binaryOperations/numpyBinaryOperations.py
@@ -0,0 +1,9 @@
+import numpy as np
+in_num1 = 10
+in_num2 = 11
+ 
+print ("Input  number1 : ", in_num1)
+print ("Input  number2 : ", in_num2) 
+   
+out_num = np.bitwise_and(in_num1, in_num2) 
+print ("bitwise_and of 10 and 11 : ", out_num) 
