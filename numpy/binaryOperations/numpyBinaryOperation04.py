4a57d3d171f820753dfd7c9f561c783346f6b8d1
slowy07 <slowy.arfy@gmail.com>
2020-12-23 06:58:50 +0700

    binary operation 04

---
+++ numpy/binaryOperations/numpyBinaryOperation04.py
@@ -0,0 +1,10 @@
+# bitwise_xor() function 
+import numpy as np
+in_num1 = 10
+in_num2 = 11
+ 
+print ("Input  number1 : ", in_num1)
+print ("Input  number2 : ", in_num2) 
+   
+out_num = np.bitwise_xor(in_num1, in_num2) 
+print ("bitwise_xor of 10 and 11 : ", out_num) 
