77621020e9e851593ff36ce5a89b52f7db03e878
slowy07 <slowy.arfy@gmail.com>
2020-12-23 06:59:12 +0700

    binary operation -5

---
+++ numpy/binaryOperations/numpyBinaryOperation05.py
@@ -0,0 +1,11 @@
+# bitwise_xor() function
+import numpy as np
+ 
+in_arr1 = [2, 8, 125]
+in_arr2 = [3, 3, 115]
+  
+print ("Input array1 : ", in_arr1) 
+print ("Input array2 : ", in_arr2)
+   
+out_arr = np.bitwise_xor(in_arr1, in_arr2) 
+print ("Output array after bitwise_xor: ", out_arr)
