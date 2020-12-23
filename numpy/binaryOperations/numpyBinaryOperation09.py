d43aa20af8e0f76eb67b0a10f30a3e61745c457a
slowy07 <slowy.arfy@gmail.com>
2020-12-23 07:00:54 +0700

    binary operation 09

---
+++ numpy/binaryOperations/numpyBinaryOperation09.py
@@ -0,0 +1,11 @@
+# right_shift() function
+ 
+import numpy as np
+in_num = 20
+bit_shift = 2
+ 
+print ("Input  number : ", in_num)
+print ("Number of bit shift : ", bit_shift ) 
+   
+out_num = np.right_shift(in_num, bit_shift) 
+print ("After right shifting 2 bit  : ", out_num) 
