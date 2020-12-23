d60fa0928fedb4921ce1d88320ee64eb8ecb2ba5
slowy07 <slowy.arfy@gmail.com>
2020-12-23 07:02:09 +0700

    binary operation 12

---
+++ numpy/binaryOperations/numpyBinaryOperation12.py
@@ -0,0 +1,12 @@
+# unpackbits() function
+import numpy as np
+ 
+# creating an array using 
+# array function
+a = np.array([[2], [7], [23]], dtype=np.uint8)
+ 
+# packing elements of an array
+# using packbits() function
+b = np.unpackbits(a, axis = 1)
+ 
+print(b)
