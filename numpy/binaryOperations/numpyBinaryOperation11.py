a5856178cf280ce60d1c7607aed76fcd3fd84ea9
slowy07 <slowy.arfy@gmail.com>
2020-12-23 07:01:49 +0700

    binary opeartion 11

---
+++ numpy/binaryOperations/numpyBinaryOperation11.py
@@ -0,0 +1,15 @@
+# packbits() function
+import numpy as np
+ 
+# creating an array using 
+# array function
+a = np.array([[[1,0,1],
+             [0,1,0]],
+             [[1,1,0],
+             [0,0,1]]])
+ 
+# packing elements of an array
+# using packbits() function
+b = np.packbits(a, axis=-1)
+ 
+print(b)
