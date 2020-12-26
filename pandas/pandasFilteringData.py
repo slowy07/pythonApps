cd2e39980cf508b6d787ff28c38641fcfc8c1b62
slowy07 <slowy.arfy@gmail.com>
2020-12-26 20:44:05 +0700

    filtering data

---
+++ pandas/pandasFilteringData.py
@@ -0,0 +1,10 @@
+import pandas as pd
+playerData = pd.read_csv('resource/playerStat.csv')
+
+#print(playerData.loc[(playerData['type'] == 'mage') & (playerData['second_type'] == 'disabler')])
+#print(playerData.loc[playerData['player_name'].str.contains('slowy07')])
+
+import re
+#print(playerData.loc[playerData['player_name'].str.contains('slowy07|catur')])
+
+print(playerData.loc[playerData['player_name'].str.contains('ab[a-z]*',flags=re.I, regex=True)])
