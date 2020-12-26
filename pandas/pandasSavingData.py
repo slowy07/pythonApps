d1f7711619b55f9704738661e756f89080bc2f67
slowy07 <slowy.arfy@gmail.com>
2020-12-26 20:45:19 +0700

    saving data

---
+++ pandas/pandasSavingData.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+
+playerData = pd.read_csv('resource/playerStat.csv')
+
+
+playerData['total'] = playerData['strengh'] + playerData['agility'] + playerData['intelligence']
+
+playerData.to_csv('resource/playerStatUpdate.csv', index=False)
+#playerData.to_excel('resource/playerStatUpdate.xls', index=False)
+#playerData.to_excel('resource/playerStatUpdate.txt', index=False)
+print(playerData.head())
