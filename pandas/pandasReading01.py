27734b7a43b57b134f10cc8095dfd7d676da9cf3
slowy07 <slowy.arfy@gmail.com>
2020-12-26 20:44:19 +0700

    reading data

---
+++ pandas/pandasReading01.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+
+countryInformation = pd.read_csv('resource/countryInformation.csv')
+
+#print columns
+#print(countryInformation.columns)
+
+#print columns spesific
+#print(countryInformation['country_name'])
+#print(countryInformation['country_name'][0:4])
+
+#otherPrint
+print(countryInformation[['country_name', 'population', 'phone_code']])
+
+
+#calling each row
+#print(countryInformation.iloc[0:2])
+
+#countryInformation[row,columns]
+#print(countryInformation.iloc[1:2])
