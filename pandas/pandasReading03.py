233908eb43923eea94f04e5661ae5c4370f20640
slowy07 <slowy.arfy@gmail.com>
2020-12-26 20:44:52 +0700

    reading chapter 03

---
+++ pandas/pandasReading03.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+countryInformation = pd.read_csv('resource/countryInformation.csv')
+
+#describe
+#print(countryInformation.describe())
+
+
+#sort_values
+#print(countryInformation.sort_values(['country_name','phone_code'], ascending=True))
+print(countryInformation.sort_values(['country_name','phone_code'], ascending=[True,82]))
+#print(countryInformation.sort_values('country_name', ascending=False))
