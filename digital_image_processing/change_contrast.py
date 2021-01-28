e27cef9ceb8eab0e0db3562f74bed8c0641135d1
slowy07 <slowy.arfy@gmail.com>
2021-01-29 01:13:55 +0700

    change contrast

---
+++ digital_image_processing/change_contrast.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def change_contrast(img: Image, level: int) -> Image:
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+
+    def contrast(c: int) -> int:
+        """
+        operation in every bit
+        """
+        return int(128 + factor * (c - 128))
+
+    return img.point(contrast)
+
+
+if __name__ == "__main__":
+    image_data = Image.open("data_image/somi.jpg")
+    contrast_image = change_contrast(image_data, 50) 
+    contrast_image.save("image_result/somi_changed_contrast.png", format="png")
