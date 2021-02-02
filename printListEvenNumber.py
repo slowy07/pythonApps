3f2fe52a1d1512cbb02d072ff28b2f6f83dd94ff
slowy07 <slowy.arfy@gmail.com>
2021-02-02 09:10:37 +0700

    fixing list

--- printListEvenNumber.py
+++ printListEvenNumber.py
@@ -1,15 +1,11 @@
-
-from builtins import list
-
 listNumber = list(map(int, input().split()))
 evenList = [i for i in listNumber if i%2 == 0]
 print(evenList)
 #exit()
 
+
 number = int(input("enter the require range :"))
 list = []
-
-
 if number < 0:
     print("not valid number, enter the positive number ")
 else:
@@ -18,3 +14,9 @@ else:
             list.append(i)
 
 print(list)
+
+"""
+>>> 20
+>>> enter the require range : 90
+>>> [2, 3, 4, 5, 6, 7, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90]
+"""
