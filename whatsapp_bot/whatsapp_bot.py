ed4740f5b821e7cecba3492d9ab8cc11404f5d56
slowy07 <slowy.arfy@gmail.com>
2021-02-02 07:08:52 +0700

    adding Add function and create birthday

--- whatsapp_bot/whatsapp_bot.py
+++ whatsapp_bot/whatsapp_bot.py
@@ -72,5 +72,135 @@ def blue_selection(event = None):
 
         phone_number = int(e3.get())
 
+        if len(str(phone_number)) != 12:
+            m1 = tk.label(window, text = "Error : please enter 12 digits of number ", fg = 'red', bg = 'black')
+            
+            canvas_1.create_window(250, 200,  window = m1)
+            m1.after(5000, m1.destroy)
+            
+        else:
+            phone_number = [phone_number]
+            driver = webdriver.Chrome(executable_path = 'chromedriver', options = option)
+            driver.get("https://web.whatsapp.com")
+            sleep(15)
+
+            def element_presence(driver, by, xpath, time):
+                element_present = EC.presence_of_all_elements_located(By.XPATH, xpath)
+                WebDriverWait(driver, time).until(element_present)
+
+            def is_connected():
+                try:
+                    socket.create_connection(("www.google.com", 80))
+                    return True
+                except:
+                    is_connected()
+
+            def send_whatsapp_message(driver, phone_no, text, no):
+                sleep(5)
+                driver.get("https://web.whatsapp.com/send?phone={}&source=&data=#".format(phone_no))
+                
+                try:
+                    sleep(7)
+                    element_presence(driver, By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]', 30)
+                    txt_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+                    for x in range(no):
+                        txt_box.send_keys(text)
+                        txt_box.send_keys("\n")
+                
+                except Exception as e:
+                    print("invalid Phone number :"+str(phone_no))
+            
+            for mobile_no in phone_number:
+                try:
+                    sleep(5)
+                    send_whatsapp_message(driver, mobile_no, message_text, no_of_message)
+                
+                except Exception as e:
+                    sleep(10)
+                    is_connected()
+    
+    c1 = tk.Button(text = 'send', command = Driver, bg = 'blue', fg = 'white', font = ('helvetica', 9, 'bold'))
+    canvas_1.create_window(250, 380, window = c1)
+
+def orange_selection(event = None):
+    with open('birthdays.json') as json_file:
+        data = json.load(json_file)
+    
+    l1 = tk.Label(window, text = "Enter message", bg = 'orange')
+    l2 = tk.Label(window, text = "Enter birth month", bg = 'orange')
+    l3 = tk.Label(Window, text = "Enter Birth day (2 digits)", bg = 'orange')
+    l4 = tk.Label(window, text = "Enter phone number", bg = 'orange')
+
+    h1 = tk.Entry(window)
+    h2 = tk.Entry(window)
+    h3 = tk.Entry(window)
+    h4 = tk.Entry(window)
+
+    canvas_1.create_window(400, 250, window = h1)
+    canvas_1.create_window(400, 290, window = h2)
+    canvas_1.create_window(400, 300, window = h3)
+    canvas_1.create_window(400, 370, window = h4)
+
+    def Driver():
+        for p in data:
+            if int(p['birth_month']) == now.strftime("%m") and int(p['birth_day']) == now.strftime("%d"):
+                driver = webdriver.chrome(executable_path = './chromedriver', options = options)
+                sleep(15)
+        
+        def element_presence(driver, by, xpath, time):
+            element_present = EC.presence_of_element_located((By.XPATH, xpath))
+            WebDriverWait(driver, time).until(element_present)
+
+        def is_connected():
+            try:
+                # connect to the host -- tells us if the host is actually
+                # reachable
+                socket.create_connection(("www.google.com", 80))
+                return True
+            except:
+                is_connected()
+        
+        def send_whatsapp_msg(driver, phone_no):
+            sleep(5)
+            driver.get("https://web.whatsapp.com/send?phone={}&source=&data=#".format(phone_no))
+
+            try:
+                sleep(7)
+                element_presence(driver, By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]', 30)
+                txt_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+                for m in data:
+                    txt_box.send_keys(m['message_text'])
+                    txt_box.send_keys("\n")
+            except Exception as e:
+                print("Invalid Phone Number:" + str(phone_no))
+
+        for mobile_no in data:
+            try:
+                print(mobile_no['birth_month'])
+                driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
+                if mobile_no['birth_month'] == now.strftime("%m") and mobile_no['birth_day'] == now.strftime("%d"):
+                    mobile_no = mobile_no['no']
+                    print(mobile_no)
+                sleep(5)
+                send_whatsapp_msg(driver, mobile_no)
+
+            except Exception as e:
+                sleep(10)
+                is_connected()    
+
+    
+    def Add():
+        import json
+        data = {}
+        data1 = []
+
+        message_text = h1.get()
+        birth_month = h2.get()
+        birth_day = h3.get()
+
+        if len(message_text) == 0 or len(birth_day) == 0 or len(birth_month) == 0:
+            m1 = tk.Label(window, text = "Error : fill the blank ", fg = 'red', bg = 'black')
+            canvas_1.create_window(250, 140, window = m1)
+            m1.after(5000, m1.destroy)
 
 window.mainloop()
