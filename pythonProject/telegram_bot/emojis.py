b1a644688869529928dae403448015b8687af777
slowy07 <slowy.arfy@gmail.com>
2021-02-15 06:20:23 +0700

    add sad emojis

--- pythonProject/telegram_bot/emojis.py
+++ pythonProject/telegram_bot/emojis.py
@@ -3,3 +3,7 @@ import random
 def happy():
     happy_emoji = ["😀","😃", "😄" ,"😁",]
     return random.choice(happy_emoji)
+
+def sad():
+    sad_emoji = ["😞" ,"😔" ,"😟" ,"😕" ,"🙁" ,"☹️"]
+    return random.choice(sad_emoji)
