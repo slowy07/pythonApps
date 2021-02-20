83e85256dafeff5f4f9960f18513792f3432a0ab
slowy07 <slowy.arfy@gmail.com>
2021-02-20 21:35:24 +0700

    array index slicing

--- numpy/arrayIndexSlicing.py
+++ numpy/arrayIndexSlicing.py
@@ -20,6 +20,30 @@ print(arr[1, 1, 2])
 
 
 #sub array using ranges
-print(arr[1:, 0:1, :2])
+print("\nsub array using ranges\n",arr[1:, 0:1, :2])
 #mixing indices and ranges
-print(arr[1:, 1, :3])
+print("\nmixing indices and ranges\n",arr[1:, 1, :3])
+
+#using fewer indices
+print("\nusing fewer indices \n",arr[1])
+
+
+"""
+[[15. 16. 17. 21.]
+ [63. 92. 36. 18.]]
+[63. 92. 36. 18.]
+36.0
+
+sub array using ranges
+ [[[15. 16.]]
+
+ [[98. 31.]]]
+
+mixing indices and ranges
+ [[63.  92.  36. ]
+ [17.  18.  19.5]]
+
+using fewer indices 
+ [[15. 16. 17. 21.]
+ [63. 92. 36. 18.]]
+"""
