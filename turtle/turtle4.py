66fee1b79016b40ff5119de019a1b9174542c2bc
slowy07 <slowy.arfy@gmail.com>
2021-02-22 18:14:59 +0700

    drawing 4

---
+++ turtle/turtle4.py
@@ -0,0 +1,15 @@
+from turtle import *
+color('purple','pink')
+def draw():
+    color('red', 'black')
+    left(60)
+    forward(100)
+    left(60)
+    forward(100)
+    left(150)
+    forward(180)
+
+
+for i in range(5):
+    draw()
+done()
