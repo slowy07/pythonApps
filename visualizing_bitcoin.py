538c5f7b7ec35754545e8eea96a76da4bbf851b1
slowy07 <slowy.arfy@gmail.com>
2021-03-01 23:44:31 +0700

    visualizing bitcoin

---
+++ visualizing_bitcoin.py
@@ -0,0 +1,28 @@
+import pandas_datareader as pd_reader
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import datetime as dt
+
+
+crypto = "BTC"
+currency = "USD"
+
+
+start_date = dt.datetime(2021, 1, 10)
+end_date = dt.datetime.now()
+
+btc = pd_reader.DataReader(f"{crypto}-{currency}", "yahoo", start_date, end_date)
+eth = pd_reader.DataReader(f"ETH-{currency}", 'yahoo', start_date, end_date)
+
+
+
+"""plt.yscale("log")"""
+plt.plot(btc['Close'], label = "BTC")
+plt.plot(eth['Close'], label = "ETH")
+plt.legend(loc = "upper left")
+plt.show()
+
+#mpf.plot(btc, type='candle', volume = True, style = 'yahoo')
+"""plt.plot(data['Close'])
+plt.show()"""
+ 
