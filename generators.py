3f5ce94e90cd1d9e0b76bb33051ee7dae8df75e8
slowy07 <slowy.arfy@gmail.com>
2021-03-01 23:34:38 +0700

    generators

---
+++ generators.py
@@ -0,0 +1,30 @@
+import sys
+
+
+
+#generators method 1
+def set_gen(x):
+    for x in range(x):
+        yield x ** 3
+"""
+values = set_gen(100)
+print(sys.getsizeof(values))
+print(next(values))
+print(next(values))
+print(next(values))
+for x in values:
+    print(x)
+"""
+
+def unlimited_seq():
+    result = 1
+    while True:
+        yield result
+        result += 5
+
+values = unlimited_seq()
+print(next(values))
+"""
+for x in values:
+    print(x)
+"""
