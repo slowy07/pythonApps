be12ef9e17587b5f7ceb355edc8bcaa70ba8109b
slowy07 <slowy.arfy@gmail.com>
2021-03-01 20:46:39 +0700

    ...

--- pythonProject/telegram_bot/main.py
+++ pythonProject/telegram_bot/main.py
@@ -3,7 +3,7 @@ import telebot
 import say_bot
 import emojis
 
-bot = telebot.TeleBot("1360881292:AAG-z5HAc88OWkjmg7yJXSDzhOokp1VMrB0", parse_mode = None)
+bot = telebot.TeleBot("", parse_mode = None)
 
 @bot.message_handler(commands = ['start', 'hello'])
 def send_message(message):
