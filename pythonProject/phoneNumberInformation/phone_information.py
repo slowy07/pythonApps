e4dd4a52edd83ea9a3bca665e7e1d2e027fab4c1
arfy slowy <slowy.arfy@gmail.com>
2021-06-04 17:12:36 +0700

    update phone number information

--- pythonProject/phoneNumberInformation/phone_information.py
+++ pythonProject/phoneNumberInformation/phone_information.py
@@ -18,3 +18,12 @@ def get_number(number_phone):
   get carrier
   """
   print(carrier.name_for_number(information, 'en'))
+  
+  #get timezone
+  print(timezone.time_zones_for_number(information))
+  
+
+phone_number = input("Enter phone number")
+print("get phone number")
+
+get_number(phone_number)
