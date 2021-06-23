611d8e42d9ae1ee9180d4f85a5b5ae39051bb09b
slowy07 <slowy.arfy@gmail.com>
2021-06-23 08:58:37 +0700

    nasa apod with request

---
+++ pythonProject/nasa_apod_with_request/main.py
@@ -0,0 +1,26 @@
+from requests.models import parse_url
+from settings import key
+import requests
+import os
+
+date = input("enter date (year-month-date) :")
+r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key}&date={date}")
+parsed = r.json()
+title = parsed['title']
+url = parsed['hdurl']
+print(f"{title} : {url}")
+
+img_ = requests.get(url, stream = True)
+print(img_.headers)
+print(img_.headers['content-type'], img_.headers['content-length'])
+content_type = img_.headers['content-type']
+
+
+if img_.status_code == 200 and content_type == 'image/jpeg' or content_type == 'image/gif' or content_type == 'image/png':
+    ext = img_.headers['content-type'][6:]
+    if not os.path.exists("img/"):
+        os.mkdir("img/")
+    path = f"img/apod_{date}.{ext}"
+    with open(path, "wb") as f:
+        for chunk in img_:
+            f.write(chunk)
