697b504167695c872ffac16fc4d3650cb89c85ab
slowy07 <slowy.arfy@gmail.com>
2021-06-30 08:49:40 +0700

    update

--- pythonProject/laundry/data/process.py
+++ pythonProject/laundry/data/process.py
@@ -19,31 +19,43 @@ class laundryinfo:
     def __init__(self):
         self.time = datetime.datetime.now()
 
-    def calculate(self, laundry_weight) -> str:
-        total = laundry_weight * 5000
+    def calculate(self, laundry_weight, type_of_laundry) ->str:
+        cuci_komplit = 6000
+        cuci_kering = 4000
+        setrika = 4000
+        bed_cover = 8000
+        # selimut = 7000
+        # gorden = 7000
+        express = 9000
+
+        if type_of_laundry in ["selimut", "gorden"]:
+            type_of_laundry = 7000
+        elif type_of_laundry == "cuci komplit":
+            type_of_laundry = cuci_komplit
+        elif type_of_laundry == "cuci kering":
+            type_of_laundry = cuci_kering
+        elif type_of_laundry == "setrika":
+            type_of_laundry = setrika
+        elif type_of_laundry == "bed cover":
+            type_of_laundry = bed_cover
+        elif type_of_laundry == express:
+            type_of_laundry = express
         
-        return f"Harga total : Rp.{total}"
+        total = type_of_laundry * int(laundry_weight)
+        
+        return int(total)
 
     def get_time(self) -> str:
         timeNow = self.time.strftime('%H:%M')
         
         return f"waktu : {timeNow}"
 
-user_info = user('arfy', 'baju 4 kg')
-harga = laundryinfo().calculate(4)
-waktu_skrng = laundryinfo().get_time()
-print(user_info.get_username())
-print(user_info.get_user_information())
-print(harga)
+
 
 def save_data():
     filename = f"laundry_{timenow.strftime('%d-%B-%Y')}.txt"
-    text_information = f"desc :{user_info.get_user_information()}\nnama : {user_info.get_username()}\nharga : {harga}\nwaktu:{waktu_skrng}\n\n"
+    text_information = f"desc :{user.get_user_information()}\nnama : {user.get_username()}\nharga : price\nwaktu: time\n\n"
     save_data_log = open(filename, 'a')
     save_data_log.write(text_information)
     save_data_log.close()
     print('saved')
-
-save_data()
-
-    
