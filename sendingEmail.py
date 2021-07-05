8791bcea2cf1bfc36979d85e1bbf365c9b3bb598
slowy07 <slowy.arfy@gmail.com>
2021-07-05 21:09:20 +0700

    update python3

--- sendingEmail.py
+++ sendingEmail.py
@@ -14,6 +14,6 @@ This is a test e-mail message.
 try:
    smtpObj = smtplib.SMTP('localhost')
    smtpObj.sendmail(sender, receivers, message)         
-   print "Successfully sent email"
-except SMTPException:
-   print "Error: unable to send email"
+   print("succes send!")
+except smtplib.SMTPException:
+   print("Error: unable to send email")
