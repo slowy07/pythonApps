5358fa8ffec4819d66f167565472997132226408
slowy07 <slowy.arfy@gmail.com>
2021-07-05 23:04:24 +0700

    .

--- pythonProject/batteryNotification/batteryNotification.py
+++ pythonProject/batteryNotification/batteryNotification.py
@@ -1,20 +1,14 @@
 #before run
 #pip install  -r requirements.txt
 import psutil #pip install psutil
+from plyer import notification
 
-battery = psutil.sensors_battery()
-plugged = battery.power_plugged
-percent = battery.percent
-
-
-if percent >= 30:
-    # pip install py-notifier
-    # pip install win10toast
-    from pynotifier import Notification
-
-    Notification(
-        title = "Battery Low"
-        description = str(percent) + "% Battery Remain !"
-        duration = 5
-        urgency = Notification.URGENCY_CRITICAL
-    ).send()
+while True:
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    notification.notify(
+        title="Battery Percentage",
+        message=str(percent)+"% Battery remaining",
+        timeout=10
+    )
+    continue
