ec7462bdcaddaf41db3b003da79fafd6539411ec
slowy07 <slowy.arfy@gmail.com>
2021-07-05 21:11:37 +0700

    update python3

--- sendingEmail.py
+++ sendingEmail.py
@@ -4,8 +4,8 @@ import smtplib
 sender = 'from@fromdomain.com'
 receivers = ['to@todomain.com']
 
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
+message = f"""From: From Person {sender}
+To: To Person {receivers}
 Subject: SMTP e-mail test
 
 This is a test e-mail message.
