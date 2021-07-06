7b9b264e9727dbb0493bb426efb809b80df089df
slowy07 <slowy.arfy@gmail.com>
2021-07-06 18:37:11 +0700

    remove unused import

--- whatsapp_bot_selenium.py
+++ whatsapp_bot_selenium.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.chrome.options import Options
 
 
