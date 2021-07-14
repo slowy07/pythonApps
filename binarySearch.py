6b03c6c6382619e4516f826330f43a6fea86c8d3
slowy07 <slowy.arfy@gmail.com>
2021-07-14 22:21:10 +0700

    adding except pass

--- binarySearch.py
+++ binarySearch.py
@@ -6,21 +6,21 @@ def binerSearch(arr, l, r, x):
         if arr[mid] == x:
             return mid
         elif arr[mid] < x:
-            l = mid + 1 
+            l = mid + 1
         elif x < arr[mid]:
             r = mid - 1
 
     return -1
 
-#main function biner search
+
+# main function biner search
 if __name__ == "__main__":
     print("Enter array with comma separated in wich element will be searched : ")
-    arr = [int(x) for x in input().split(',')]
+    arr = [int(x) for x in input().split(",")]
     x = eval(input("enter the element you want search given array :"))
 
+    result = binerSearch(arr, 0, len(arr), -1, x)  # lgtm [py/call/wrong-arguments]
 
-    result = binerSearch(arr, 0, len(arr), -1, x)
-    
     if result != -1:
         print("element in present index {}".format(result))
     else:
