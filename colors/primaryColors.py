c9e709eaf65657588b6f98f77f0e7b840b1da2e9
slowy07 <slowy.arfy@gmail.com>
2021-07-31 07:14:39 +0700

    reformatted

--- colors/primaryColors.py
+++ colors/primaryColors.py
@@ -1,28 +1,28 @@
 def diff(a, b):
-    return a -b
+    return a - b
 
 
-def simpleColor(r,g,b):
+def simpleColor(r, g, b):
     r = int(r)
     g = int(g)
     b = int(b)
     bg = ir = 0
     try:
         if r > g and r > b:
-            rg = diff(r,g)
-            rb = diff(r,b)
-            
+            rg = diff(r, g)
+            rb = diff(r, b)
+
             if g < 65 and b < 65 and rg < 60:
                 return "RED"
-            
-            gb = diff(g,b)
+
+            gb = diff(g, b)
             if rg < rb:
                 if gb < rg:
-                    if gb >=30 and rg >=80:
+                    if gb >= 30 and rg >= 80:
                         return "ORANGE"
-                    elif gb <=20 and rg >= 80:
+                    elif gb <= 20 and rg >= 80:
                         return "RED"
-                    elif gb <=20 and b > 175:
+                    elif gb <= 20 and b > 175:
                         return "CREAM"
                     else:
                         return "COCHOLATE"
@@ -62,18 +62,18 @@ def simpleColor(r,g,b):
                         return "MARRON"
                 else:
                     return "GRAY"
-        
+
         elif g > r and g > b:
-            gb = diff(g,b)
-            gr = diff(g,r)
-            
+            gb = diff(g, b)
+            gr = diff(g, r)
+
             if r < 65 and b < 65 and gb > 60:
                 return "GREEN"
-            
-            rb = diff(r,b)
+
+            rb = diff(r, b)
             if r > b:
                 if gr < gb:
-                    if rb >=150 and gr <= 20:
+                    if rb >= 150 and gr <= 20:
                         return "AMARILLO"
                     else:
                         return "GREEN"
@@ -87,15 +87,15 @@ def simpleColor(r,g,b):
                         return "GREEN"
                 else:
                     return "GREEN"
-        
+
         elif b > r and b > g:
-            bg = diff(b,g)
-            br = diff(b,r)
+            bg = diff(b, g)
+            br = diff(b, r)
 
             if r < 65 and g < 65 and bg > 60:
                 return "BLUE"
-            
-            rg = diff(r,g)
+
+            rg = diff(r, g)
             if g > r:
                 if bg > rg:
                     if bg <= 20:
@@ -137,14 +137,15 @@ def simpleColor(r,g,b):
                         return "MARRON"
                 else:
                     return "GRAY"
-            
+
         else:
             return "GRAY"
-    
+
     except:
         return "Not Color"
 
 
 if __name__ == "__main__":
     import sys
+
     print(simpleColor(sys.argv[1], sys.argv[2], sys.argv[3]))
