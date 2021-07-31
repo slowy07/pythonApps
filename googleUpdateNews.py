01a3053a99ec8cf59804cfa7d2e6babc07cfe5ed
slowy07 <slowy.arfy@gmail.com>
2021-07-31 07:33:26 +0700

    reformatted code

--- googleUpdateNews.py
+++ googleUpdateNews.py
@@ -9,22 +9,21 @@ https://pypi.org/project/beautifulsoup4/
 from bs4 import BeautifulSoup as soup
 
 
-def news(xmlNewsUrl,counter):
+def news(xmlNewsUrl, counter):
     context = ssl._create_unverified_context()
     client = urlopen(xmlNewsUrl, context=context)
     xmlPage = client.read()
     client.close()
 
-
     soupPage = soup(xmlPage, "xml")
     newsList = soupPage.findAll("item")
     i = 0
-    
+
     for news in newsList:
-        print(f'news title:   {news.title.text}')    # to print title of the news
-        print(f'news link:    {news.link.text}')     # to print link of the news
-        print(f'news pubDate: {news.pubDate.text}') # to print published date
-        
+        print(f"news title:   {news.title.text}")  # to print title of the news
+        print(f"news link:    {news.link.text}")  # to print link of the news
+        print(f"news pubDate: {news.pubDate.text}")  # to print published date
+
         print("+-" * 20, "\n\n")
 
         if i == counter:
@@ -32,9 +31,10 @@ def news(xmlNewsUrl,counter):
         else:
             i = i + 1
 
+
 newsUrl = "https://news.google.com/news/rss/?ned=us&gl=US&hl=en"
 sportUrl = "https://news.google.com/news/rss/headlines/section/topic/SPORTS.en_in/Sports?ned=in&hl=en-IN&gl=IN"
 
-#print news
+# print news
 news(newsUrl, 10)
 news(sportUrl, 5)
