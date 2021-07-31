2082e65b6ab23bfb0b5973bba331b37b6fa00019
slowy07 <slowy.arfy@gmail.com>
2021-07-31 07:32:15 +0700

    fixing

--- generators.py
+++ generators.py
@@ -1,7 +1,9 @@
-#generators method 1
+# generators method 1
 def set_gen(x):
     for x in range(x):
         yield x ** 3
+
+
 """
 values = set_gen(100)
 print(sys.getsizeof(values))
@@ -12,11 +14,13 @@ for x in values:
     print(x)
 """
 
+
 def unlimited_seq():
     result = 1
     while True:
         yield result
         result += 5
+
 
 values = unlimited_seq()
 print(next(values))
