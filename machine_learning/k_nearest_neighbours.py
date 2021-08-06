9b45d40a355c83f4a3e103e07cd16e59328c4984
slowy07 <slowy.arfy@gmail.com>
2021-08-06 22:20:52 +0700

    refixing doctest

--- machine_learning/k_nearest_neighbours.py
+++ machine_learning/k_nearest_neighbours.py
@@ -10,14 +10,14 @@ x = np.array(data['data'])
 y = np.array(data['target'])
 classes = data["target_names"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(x, y)
 
 def euclidan_distance(a, b):
     """
     Gives the euclidean distance between two points
-    >>> euclidean_distance([0, 0], [3, 4])
+    >>> euclidan_distance([0, 0], [3, 4])
     5.0
-    >>> euclidean_distance([1, 2, 3], [1, 8, 11])
+    >>> euclidan_distance([1, 2, 3], [1, 8, 11])
     10.0
     """
     return np.linalg.norm(np.array(a) - np.array(b))
@@ -42,7 +42,7 @@ def classifier(train_data, train_target, classes, point, k = 5):
     distances = []
     for data_point in data:
         distance = euclidan_distance(data_point[0], point)
-        distances.append(distance, data_point[1])
+        distances.append((distance, data_point[1]))
 
     votes = [i[1] for i in sorted(distances)[:k]]
     result = Counter(votes).most_common(1)[0][0]
@@ -50,4 +50,6 @@ def classifier(train_data, train_target, classes, point, k = 5):
     return classes[result]
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     print(classifier(X_train, y_train, classes, [4.4, 3.1, 1.3, 1.4]))
