fe8e516c5cb69741a850931ace05163ab50c2094
arfy slowy <slowy.arfy@gmail.com>
2021-08-19 16:55:33 +0700

    update patch to comment

--- webBlocker.py
+++ webBlocker.py
@@ -6,19 +6,21 @@ redirect = "127.0.0.1"
 websites = ["www.facebook.com", "https://www.facebook.com"]  
   
 while True:  
-    if datetime(datetime.now().year,datetime.now().month,datetime.now().day,9)<datetime.now()<datetime(datetime.now().year,datetime.now().month,datetime.now().day,17):  
-        with open(host_path,"r+") as fileptr:  
-            content = fileptr.read()  
-            for website in websites:  
-                if website in content:  
-                    pass  
-                else:  
-                    fileptr.write(redirect+"    "+website+"\n")  
-    else:  
-        with open(host_path,'r+') as file:  
-            content = file.readlines() 
-            file.seek(0)  
-            for line in content:  
-                if not any(website in line for website in websites):  
-                    file.write(line)  
-            file.truncate()  
+    # if datetime(datetime.now().year,datetime.now().month,datetime.now().day,9)<datetime.now()<datetime(datetime.now().year,datetime.now().month,datetime.now().day,17):  
+    with open(host_path,"r+") as fileptr:
+        content = fileptr.read()  
+        for website in websites:
+            if website in content:
+                pass
+                print("web was blocked")
+            else:
+                print(f"blocking website {a for a in websites}")
+                fileptr.write(redirect+"    "+website+"\n")  
+    # else:
+    #     with open(host_path,'r+') as file:  
+    #         content = file.readlines() 
+    #         file.seek(0)  
+    #         for line in content:  
+    #             if not any(website in line for website in websites):  
+    #                 file.write(line)  
+    #         file.truncate()  
