c2b6cc794854756efee9ca65bae4f8577b8d4b6a
arfy slowy <slowy.arfy@gmail.com>
2021-11-20 21:18:44 +0700

    Create majority_element.py

---
+++ leetcode_solution/majority_element.py
@@ -0,0 +1,9 @@
+import collections
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return collections.Counter(nums).most_common(1)[0][0]
