4c33cd0ec90ebb57d0c6e8a1ab2e0c00e8815b59
arfy slowy <slowy.arfy@gmail.com>
2021-11-20 20:48:23 +0700

    Create compare_version_numbers.py

---
+++ leetcode_solution/compare_version_numbers.py
@@ -0,0 +1,28 @@
+import itertools
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        n1, n2 = len(version1), len(version2)
+        i, j = 0, 0
+        
+        while i < n1 or j < n2:
+            v1, v2 = 0, 0
+            while i < n1 and version1[i] != '.':
+                v1 = v1 * 10 + int(version1[i])
+                i += 1
+            while j < n2 and version2[j] != '.':
+                v2 = v2 * 10 + int(version2[j])
+                j += 1
+            if v1 != v2:
+                return 1 if v1 > v2 else -1
+            
+            i += 1
+            j += 1
+            
+        return 0
+        
