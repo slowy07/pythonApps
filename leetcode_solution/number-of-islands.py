37fa1c26309b0bc1107939afd9841b2167b3468e
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 08:01:51 +0700

    dfs

---
+++ leetcode_solution/number-of-islands.py
@@ -0,0 +1,33 @@
+import collections
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        def bfs(grid, i, j):
+            if grid[i][j] == '0':
+                return False
+            grid[i][j] ='0'
+            q = collections.deque([(i, j)])
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r+dr, c+dc
+                    if not (0 <= nr < len(grid) and
+                            0 <= nc < len(grid[0]) and
+                            grid[nr][nc] == '1'):
+                        continue
+                    grid[nr][nc] = '0'
+                    q.append((nr, nc))
+            return True
+
+        count = 0
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                if bfs(grid, i, j):
+                    count += 1
+        return count
+        
