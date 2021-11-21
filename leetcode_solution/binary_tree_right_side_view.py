e991a3d13304d01f510d43dcfabe649df30a9f0d
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 07:59:53 +0700

    Create binary_tree_right_side_view.py

---
+++ leetcode_solution/binary_tree_right_side_view.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        result, current = [], [root]
+        
+        while current:
+            next_level = []
+            for node in current:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+                
+            result.append(node.val)
+            current = next_level
+            
+        return result
