3476249cd171001f0a17e4c0da4aa54da3a124fa
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 18:49:02 +0700

    Create bitwise_and_of_numbers_range.py

---
+++ leetcode_solution/bitwise_and_of_numbers_range.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        while left < right:
+            right &= right - 1
+        return right
+        
