6f43e91fb6afe947ffc9ea19a0c221fb733fd73f
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 07:55:49 +0700

    Create house_robber.py

---
+++ leetcode_solution/house_robber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        last, now = 0, 0
+        for i in nums:
+            last, now = now, max(last + i, now)
+            
+        return now
