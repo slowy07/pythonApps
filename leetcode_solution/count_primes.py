e1b18b3f5bf1a470e2bfe8faf696d36a00249604
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 19:03:50 +0700

    Create count_primes.py

---
+++ leetcode_solution/count_primes.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return 0
+        
+        is_prime = [True] * (n // 2)
+        cnt = len(is_prime)
+        for i in xrange(3, n, 2):
+            if i * i >= n:
+                break
+            if not is_prime[i // 2]:
+                continue
+            for j in xrange(i * i, n, 2* i):
+                if not is_prime[j // 2]:
+                    continue
+                cnt -= 1
+                is_prime[j // 2] = False
+        return cnt
