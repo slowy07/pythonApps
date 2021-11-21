5b13538fe8876f60f18dc81b9ba191cd952710af
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 07:39:46 +0700

    Create repeated_dna_sequences.py

---
+++ leetcode_solution/repeated_dna_sequences.py
@@ -0,0 +1,13 @@
+import collections
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        l, r = [], []
+        if len(s) < 10: return []
+        for i in range(len(s) - 9):
+            l.extend([s[i:i + 10]])
+        return [k for k, v in collections.Counter(l).items() if v > 1]
