d046e88d7adfe68540b650a2e45b61c014e81f77
arfy slowy <slowy.arfy@gmail.com>
2021-11-21 07:40:28 +0700

    Create best_time_to_buy_and_sell_iv.py

---
+++ leetcode_solution/best_time_to_buy_and_sell_iv.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def maxProfit(self, k, prices):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0 
+        
+        K = k+1
+        n = len(prices)
+        
+        if k>=n//2:
+            maxProf = 0
+            buy = prices[0]
+            for i in range(1, n):
+                maxProf +=max(0, prices[i]-prices[i-1])
+            return maxProf
+                
+        dp =[[0]*n for _ in range(K)]
+        
+        for i in range(1, K):
+            buy = -prices[0]
+            for j in range(1, n):
+                dp[i][j] = max(dp[i][j], prices[j]+buy, dp[i][j-1])
+                buy = max(buy, -prices[j]+dp[i-1][j-1])
+        return dp[-1][-1]
+        
