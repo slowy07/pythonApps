9c342dac018301d08ca7b9c8a54897f76945b1cf
arfy slowy <slowy.arfy@gmail.com>
2021-11-22 19:29:18 +0700

    dfs solution

---
+++ leetcode_solution/course_schedule.py
@@ -0,0 +1,29 @@
+import collections
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        in_degree = collections.defaultdict(set)
+        out_degree = collections.defaultdict(set)
+        
+        for i, j in prerequisites:
+            in_degree[i].add(j)
+            out_degree[j].add(i)
+            
+        stk = [i for i in xrange(numCourses) if i not in in_degree]
+        
+        while stk:
+            node = stk.pop()
+            for i in out_degree[node]:
+                in_degree[i].remove(node)
+                if not in_degree[i]:
+                    stk.append(i)
+                    del in_degree[i]
+                    
+            del out_degree[node]
+        
+        return not in_degree and not out_degree
