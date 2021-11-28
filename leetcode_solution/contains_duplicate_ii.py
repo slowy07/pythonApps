71afbefca476d4f320f9e3f9f0cd10b94d107442
arfy slowy <slowy.arfy@gmail.com>
2021-11-28 09:50:07 +0700

    Create contains_duplicate_ii.py

---
+++ leetcode_solution/contains_duplicate_ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            if num not in lookup:
+                lookup[num] = i
+            else:
+                if i - lookup[num] <= k:
+                    return True
+                
+                lookup[num] = i
+            
+        return False
+        
