5d4fd73b6f0f2fc180f3130a78f6d76417fc7915
arfy slowy <slowy.arfy@gmail.com>
2021-12-02 21:29:58 +0700

    Create rectangle_area.py

---
+++ leetcode_solution/rectangle_area.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type ax1: int
+        :type ay1: int
+        :type ax2: int
+        :type ay2: int
+        :type bx1: int
+        :type by1: int
+        :type bx2: int
+        :type by2: int
+        :rtype: int
+        """
+        return (D - B) * (C - A) + \
+               (G - E) * (H - F) - \
+               max(0, (min(C, G) - max(A, E))) * \
+               max(0, (min(D, H) - max(B, F)))
+         
