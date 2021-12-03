dd016d0848c9dc88df7b2007894a94a4b31b6645
arfy slowy <slowy.arfy@gmail.com>
2021-12-03 21:48:45 +0700

    one line

---
+++ leetcode_solution/majority_element_ii.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        return [i for i, j in Counter(nums).items() if j > len(nums) // 3]
+        
