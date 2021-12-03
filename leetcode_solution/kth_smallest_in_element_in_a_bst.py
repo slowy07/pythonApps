e2164bf7a6436dd3fd08304752aa81330e6bc15d
arfy slowy <slowy.arfy@gmail.com>
2021-12-03 21:53:09 +0700

    Create kth_smallest_in_element_in_a_bst.py

---
+++ leetcode_solution/kth_smallest_in_element_in_a_bst.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        s, cur, rank = [], root, 0
+        while s or cur:
+            if cur:
+                s.append(cur)
+                cur = cur.left
+                
+            else:
+                cur = s.pop()
+                rank += 1
+                if rank == k:
+                    return cur.val
+                cur = cur.right
+                
+        return float("-inf")
