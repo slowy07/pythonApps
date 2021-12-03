ddaa1fc69bb0fa40f7f26b65a55923b7a29a649f
arfy slowy <slowy.arfy@gmail.com>
2021-12-03 22:03:39 +0700

    Create implment_queue_using_stacks.py

---
+++ leetcode_solution/implment_queue_using_stacks.py
@@ -0,0 +1,46 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.A, self.B = [], []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.A.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        self.peek()
+        return self.B.pop()
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.B:
+            while self.A:
+                self.B.append(self.A.pop())
+        return self.B[-1]
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.A and not self.B
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
