79a4ae13aad7c98f2b740b2a1bcb4817cebe5fb5
arfy slowy <slowy.arfy@gmail.com>
2021-12-03 22:12:52 +0700

    Create number_of_digits.py

---
+++ leetcode_solution/number_of_digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        res = 0
+        cur = 1
+        while cur <= n:
+            res += ((n // (cur * 10)) * cur) + min(max( n % (cur * 10) - cur + 1, 0), cur)
+        
+            cur *= 10
+        
+        return res
