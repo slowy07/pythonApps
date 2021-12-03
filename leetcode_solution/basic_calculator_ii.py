189a8c5560eba3b98b4e93e24cd45a09b5e7e60a
arfy slowy <slowy.arfy@gmail.com>
2021-12-03 21:32:36 +0700

    Create basic_calculator_ii.py

---
+++ leetcode_solution/basic_calculator_ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        op = {"*": lambda x, y: float(x)*y, "/": lambda x, y: float(x) / y}
+        
+        stack, tot, prevOp = [0], 0, "+"
+        for j, char in enumerate(s):
+            if char.isdigit():
+                tot = tot * 10 + int(char)
+            if j == len(s) - 1 or not char.isdigit() and char != " ":
+                if prevOp in ["/", "*"]:
+                    calc = op[prevOp](stack[-1], tot)
+                    stack[-1] = int(math.floor(calc) if calc > 0 else math.ceil(calc))
+                else:
+                    stack.append(-tot if prevOp == '-' else tot)
+                    
+                tot,prevOp = 0, char
+                
+        return sum(stack)
