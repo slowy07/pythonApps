a95be2f154c6bd2e73ac3d3cb8d7e8a58dfa5f2a
arfy slowy <slowy.arfy@gmail.com>
2021-12-04 12:19:37 +0700

    Create product_array_except_self.py

---
+++ leetcode_solution/product_array_except_self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        m, res = 1, []
+        for i in range(len(nums)):
+            res.append(m)
+            m *= nums[i]
+            
+        m = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= m
+            m *= nums[i]
+            
+        return res
+        
