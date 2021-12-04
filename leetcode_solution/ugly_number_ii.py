b7c972c64ac5a0cd7aa71e806bca359b89632cdb
arfy slowy <slowy.arfy@gmail.com>
2021-12-04 23:03:47 +0700

    Create ugly_number_ii.py

---
+++ leetcode_solution/ugly_number_ii.py
@@ -0,0 +1,22 @@
+import heapq
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ugly_number = 0
+        heap = []
+        heapq.heappush(heap, 1)
+        for _ in xrange(n):
+            ugly_number = heapq.heappop(heap)
+            if ugly_number % 2 == 0:
+                heapq.heappush(heap, ugly_number * 2)
+            elif ugly_number % 3 == 0:
+                heapq.heappush(heap, ugly_number * 2)
+                heapq.heappush(heap, ugly_number * 3)
+            else:
+                heapq.heappush(heap, ugly_number * 2)
+                heapq.heappush(heap, ugly_number * 3)
+                heapq.heappush(heap, ugly_number * 5)
+        return ugly_number
