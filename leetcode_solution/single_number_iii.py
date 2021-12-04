d5ac40cf24091a11c67f30ea480cc482b2b68bb5
arfy slowy <slowy.arfy@gmail.com>
2021-12-04 22:43:39 +0700

    Create single_number_iii.py

---
+++ leetcode_solution/single_number_iii.py
@@ -0,0 +1,13 @@
+import operator
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        x_xor_y = reduce(operator.xor, nums)
+        bit  = x_xor_y & -x_xor_y
+        result = [0, 0]
+        for i in nums:
+            result[bool(i & bit)] ^= i
+        return result
