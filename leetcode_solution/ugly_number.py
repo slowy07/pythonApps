1c82dd77fa2ff87e442462e6386baabcca775876
arfy slowy <slowy.arfy@gmail.com>
2021-12-04 22:57:10 +0700

    Create ugly_number.py

---
+++ leetcode_solution/ugly_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if num == 0:
+            return False
+        for i in [2,3,5]:
+            while num % i == 0:
+                num /= i
+        
+        return num == 1
