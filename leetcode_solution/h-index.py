2485755f9ce1d5b21672e293e932560b9533ae27
arfy slowy <slowy.arfy@gmail.com>
2021-12-05 09:07:20 +0700

    Create h-index.py

---
+++ leetcode_solution/h-index.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse=True)
+        h = 0
+        for x in citations:
+            if x >= h + 1:
+                h += 1
+            else:
+                break
+        
+        return h
+        
