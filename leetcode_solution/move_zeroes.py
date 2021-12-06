31b23dcb170718801274ed11b5a3ccc73156121d
arfy slowy <slowy.arfy@gmail.com>
2021-12-06 20:08:02 +0700

    Create move_zeroes.py

---
+++ leetcode_solution/move_zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[pos] = nums[i]
+                pos += 1
+                
+        for i in xrange(pos, len(nums)):
+            nums[i] = 0
