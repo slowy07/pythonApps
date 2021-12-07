e1cf2b583b7493c47128009d100e2d2a2936ee44
arfy slowy <slowy.arfy@gmail.com>
2021-12-07 07:46:40 +0700

    Create longlest_increasing_subsequence.py

---
+++ leetcode_solution/longlest_increasing_subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        l = len(nums)
+        if l == 1:
+            return 1
+        
+        @lru_cache(maxsize=None)
+        def go(i):
+            n = nums[i]
+            
+            return 1 + max((go(j) for j in range(i + 1, l) if nums[j] > n), default=0)
+        
+        return max(go(i) for i in range(l))
+        
