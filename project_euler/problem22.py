0297f2ea2ca4715262dc97b0cc116f7732530188
slowy07 <slowy.arfy@gmail.com>
2021-12-09 18:06:14 +0700

    feat: adding solution22
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ project_euler/problem22.py
@@ -0,0 +1,26 @@
+import os
+
+
+def solution() -> None:
+    """
+    >>> solution()
+    871198282
+    """
+    total_sum = 0
+    temp_sum = 0
+    with open(os.path.dirname(__file__) + "problem22.txt") as file:
+        name = str(file.readlines()[0])
+        name = name.replace('"', "").split(",")
+
+    name.sort()
+    for i in range(len(name)):
+        for j in name[i]:
+            temp_sum += ord(j) - ord("A") + 1
+
+        total_sum += (i + 1) * temp_sum
+        temp_sum = 0
+
+    return total_sum
+
+
+print(solution())
