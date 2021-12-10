92962c30682fdb8834d3f1328fd3094b8cfff177
slowy07 <slowy.arfy@gmail.com>
2021-12-10 10:39:25 +0700

    feat: shannon_entropy
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ shannon_entropy.py
@@ -0,0 +1,18 @@
+import collections
+import math
+
+def estimate_shannon_entropy(dna_sequece):
+    m = len(dna_sequece)
+    bases = collections.Counter([tmp_base for tmp_base in dna_sequece])
+    
+    shannon_entropy_value = 0
+    for base in bases:
+        n_i = bases[base]
+        p_i = n_i / float(m)
+
+        entropy_i = p_i * (math.log(p_i, 2))
+        shannon_entropy_value += entropy_i
+
+    return shannon_entropy_value * -1
+
+print(estimate_shannon_entropy("ATCTAGGAC"))
