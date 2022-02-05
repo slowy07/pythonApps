dbcb4b44bb394fd2c5ae158b42526c5a194c40eb
arfy slowy <slowy.arfy@gmail.com>
2022-02-05 19:19:23 +0700

    Create patching_array.py

---
+++ leetcode_solution/patching_array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        patch, miss, i = 0, 1, 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patch += 1
+        
+        return patch
+        
