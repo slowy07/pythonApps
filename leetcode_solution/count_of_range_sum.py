abe7bf223d7c4cd7b32541866125dab9ee0c4a2f
arfy slowy <slowy.arfy@gmail.com>
2022-02-05 19:03:29 +0700

    Update count_of_range_sum.py

--- leetcode_solution/count_of_range_sum.py
+++ leetcode_solution/count_of_range_sum.py
@@ -38,3 +38,34 @@ class Solution(object):
         for i in xrange(len(nums)):
             sums[i + 1] = sums[i] + nums[i]
         return countAndMergeSort(sums, 0, len(sums), lower, upper)
+    
+    
+#     class Solution2(object):
+#     def countRangeSum(self, nums, lower, upper):
+#         """
+#         :type nums: List[int]
+#         :type lower: int
+#         :type upper: int
+#         :rtype: int
+#         """
+#         first = [0]
+#         for num in nums:
+#             first.append(first[-1] + num)
+        
+#         def sort(lo, hi):
+#             mid = (lo + hi) / 2
+#             if mid == lo:
+#                 return 0
+#             count = sort(lo, mid) + sort(mid, hi)
+#             i = j = mid
+#             for left in first[lo:mid]:
+#                 while i < hi and first[i] - left < lower:
+#                     i += 1
+#                 while j < hi and first[j] - left <= upper:
+#                     j += 1
+                
+#                 count += j - i
+#             first[lo:hi] = sorted(first[lo:hi]) 
+#             return count
+        
+#         return sort(0, len(first))
