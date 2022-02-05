2cb2289c5af99012c1975087e4d89cd3cf24d2ca
arfy slowy <slowy.arfy@gmail.com>
2022-02-05 19:47:39 +0700

    Create reconstruct_itinerary.py

---
+++ leetcode_solution/reconstruct_itinerary.py
@@ -0,0 +1,26 @@
+import collections
+
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        adj = collections.defaultdict(list)
+        for ticket in tickets:
+            adj[ticket[0]].append(ticket[1])
+        for x in adj.itervalues():
+            x.sort(reverse = True)
+            
+        origin = "JFK"
+        result = []
+        stk = [origin]
+        while stk:
+            while adj[stk[-1]]:
+                stk.append(adj[stk[-1]].pop())
+                
+            result.append(stk.pop())
+        result.reverse()
+        return result
+
+        
