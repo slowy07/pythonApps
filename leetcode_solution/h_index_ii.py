5b753da4a92eda6b394fe01b3ab734ead88dc786
arfy slowy <slowy.arfy@gmail.com>
2022-02-05 19:11:05 +0700

    Update h_index_ii.py

--- leetcode_solution/h_index_ii.py
+++ leetcode_solution/h_index_ii.py
@@ -14,3 +14,21 @@ class Solution(object):
                 left = mid + 1
 
         return n - left
+    
+class Solution2(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # f(A)=10, f(B)=8, f(C)=5, f(D)=4, f(E)=3　→ h-index=4
+        # f(A)=25, f(B)=8, f(C)=5, f(D)=3, f(E)=3　→ h-index=3
+        lo, hi = 0, len(citations)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if citations[mid] < len(citations) - mid:
+                lo = mid + 1
+            else:
+                hi = mid
+                
+        return len(citations) - lo
