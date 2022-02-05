356cb0e69ec4b60b41708a4c71417350fbe7a0c5
arfy slowy <slowy.arfy@gmail.com>
2022-02-05 19:50:56 +0700

    Create increasing_triplet_subsequence.py

---
+++ leetcode_solution/increasing_triplet_subsequence.py
@@ -0,0 +1,17 @@
+import bisect
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        min_num, a, b = float("inf"), float("inf"), float("inf")
+        for c in nums:
+            if min_num >= c:
+                min_num = c
+            elif b >= c:
+                a, b = min_num, c
+            else:
+                return True
+            
+        return False
