2076ee0ee1259451680d420927f5d8e35e3d313b
arfy slowy <slowy.arfy@gmail.com>
2022-02-12 11:18:54 +0700

    Create valid_perfect_square.py

---
+++ leetcode_solution/valid_perfect_square.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        left, right = 1, num
+        while left <= right:
+            mid = left + (right - left) / 2
+            if mid >= num / mid:
+                right = mid - 1
+            else:
+                left = mid + 1                
+        return left == num / left and num % left  == 0
+        
