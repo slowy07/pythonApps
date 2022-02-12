780fb071d5a1b2d033d2f3a9b5c02559fe1ede58
arfy slowy <slowy.arfy@gmail.com>
2022-02-12 11:59:13 +0700

    Create find_k_pairs_with_smalles_sums.py

---
+++ leetcode_solution/find_k_pairs_with_smalles_sums.py
@@ -0,0 +1,32 @@
+from heapq import heappush, heappop
+
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        pairs = []
+        if len(nums1) > len(nums2):
+            tmp = self.kSmallestPairs(nums2, nums1, k)
+            for pair in tmp:
+                pairs.append([pair[1], pair[0]])
+            return pairs
+        
+        min_heap = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heappush(min_heap, [nums1[i] + nums2[j], i, j])
+        
+        push(0, 0)
+        while min_heap and len(pairs) < k:
+            _, i, j = heappop(min_heap)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        
+        return pairs
+        
