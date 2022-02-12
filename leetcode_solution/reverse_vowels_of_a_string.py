60d83e0f0c38b7c4ef05ad7b257dac266bbb924c
arfy slowy <slowy.arfy@gmail.com>
2022-02-12 10:40:27 +0700

    Create reverse_vowels_of_a_string.py

---
+++ leetcode_solution/reverse_vowels_of_a_string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = "aeiou"
+        string = list(s)
+        i, j = 0, len(s) - 1
+        while i < j:
+            if string[i].lower() not in vowels:
+                i += 1
+            elif string[j].lower() not in vowels:
+                j -= 1
+            else:
+                string[i], string[j] = string[j], string[i]
+                i += 1
+                j -= 1
+        
+        return "".join(string)
