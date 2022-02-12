02691dceeff685374eaca78d7352107792e77574
arfy slowy <slowy.arfy@gmail.com>
2022-02-12 10:43:27 +0700

    Create top_k_frequent_element.py

---
+++ leetcode_solution/top_k_frequent_element.py
@@ -0,0 +1,23 @@
+import collections
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counts = collections.Counter(nums)
+        buckets = [[] for _ in xrange(len(nums) + 1)]
+        for i, count in counts.iteritems():
+            buckets[count].append(i)
+            
+        result = []
+        for i in reversed(xrange(len(buckets))):
+            for j in xrange(len(buckets[i])):
+                result.append(buckets[i][j])
+                if len(result) == k:
+                    return result
+        
+        return result
+        
