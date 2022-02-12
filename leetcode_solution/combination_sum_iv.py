eab29674812f2d68100836ad636301f82b509fe1
arfy slowy <slowy.arfy@gmail.com>
2022-02-12 12:09:28 +0700

    Create combination_sum_iv.py

---
+++ leetcode_solution/combination_sum_iv.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        nums.sort()
+        
+        for i in xrange(1, target + 1):
+            for j in xrange(len(nums)):
+                if nums[j] <= i:
+                    dp[i] += dp[i - nums[j]]
+                else:
+                    break
+                    
+        return dp[target]
+        
